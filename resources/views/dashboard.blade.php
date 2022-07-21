535b3c39f30d9bed14ddcfcb7acb8b298fde41f7
YudiAdisatria <yudi.adisatria@gmail.com>
2022-07-21 15:20:27 +0700

    Menambahkan card manual book, script

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -23,6 +23,15 @@
                     </div>
                 </div>
             </div>
+
+            <div class="w-full px-4 lg:w-1/2 xl:w-1/3">
+                <div class="bg-blue-600 rounded-xl shadow-lg overflow-hidden mb-10">
+                    <div class="py-8 px-6">
+                        <h3 class="mb-3 font-bold text-xl text-white text-center">Manual Book Website</h3>
+                        <p class="text-center"><a href="{{ asset('storage\') }}" class="items-center font-bold text-sm text-dark bg-white py-2 px-4 rounded-lg hover:opacity-80 mr-50">Download di Sini</a></p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </x-app-layout>
--- serve.bat
+++ serve.bat
@@ -1,3 +1,3 @@
 cd D:\Yudi\project\project614
-
+start chrome "http://localhost:8000"
 php artisan serve
