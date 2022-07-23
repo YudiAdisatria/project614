0d6c152bbffd98fb42f12a190ba8ee183909996e
YudiAdisatria <yudi.adisatria@gmail.com>
2022-07-23 10:49:48 +0700

    Memperbaiki edit kompetensi

--- resources/views/kompetensi/edit.blade.php
+++ resources/views/kompetensi/edit.blade.php
@@ -32,8 +32,9 @@
                                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
                                     Nama Profil Kompetensi
                                 </label>
-                                <input disabled value="{{ old('id') ?? $kompetensi->id }}" name="id" class="appearance-none block w-full bg-white text-gray-700 border border-gray-100 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text">
-                                <input value="{{ old('profil') ?? $kompetensi->profil }}" name="profil" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="hidden" placeholder="Nama Profil Kompetensi">
+                                <input value="{{ old('id') ?? $kompetensi->id }}" name="id" class="appearance-none block w-full bg-white text-gray-700 border border-gray-100 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="hidden">
+                                <input value="{{ old('profil') ?? $kompetensi->profil }}" name="profil" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="hidden">
+                                <input disabled value="{{ old('profil') ?? $kompetensi->profil }}" name="profil" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Nama Profil Kompetensi">
                             </div>
                         
                         
