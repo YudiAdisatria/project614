14f654706a1d56f493a9841ffe29c4581c0a425b
YudiAdisatria <93643864+YudiAdisatria@users.noreply.github.com>
2022-08-05 22:04:04 +0700

    Delete db_event directory

--- db_event/viewIPK.sql
+++
@@ -1,9 +0,0 @@
-DROP VIEW IF EXISTS vwIPK;
-CREATE VIEW vwIPK AS
-
-SELECT mahasiswas.nim,
-sum(nilais.nilai*matkuls.sks)/sum(matkuls.sks) AS IPK
-
-FROM mahasiswas INNER JOIN nilais ON(mahasiswas.nim=nilais.nim) 
-INNER JOIN matkuls ON (nilais.kode_matkul=matkuls.kode_matkul)
-GROUP BY mahasiswas.nim;
